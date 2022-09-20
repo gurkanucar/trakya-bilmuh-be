a1a0ec998fff5ed01b4528d0bc55042842798ff9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-20 23:40:23 +0300

    User model updated

--- src/main/java/com/gucardev/trakyabilmuhbe/model/User.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/model/User.java
@@ -1,7 +1,10 @@
 package com.gucardev.trakyabilmuhbe.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -11,6 +14,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "`user`")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User extends BaseEntity {
 
     private String username;
