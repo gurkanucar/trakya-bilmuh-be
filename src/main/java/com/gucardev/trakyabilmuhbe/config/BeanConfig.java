be1cb1190bbea4c2327efdb56875ddab281ebaae
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-20 22:24:59 +0300

    model mapper implemented and added to bean

--- src/main/java/com/gucardev/trakyabilmuhbe/config/BeanConfig.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/config/BeanConfig.java
@@ -1,5 +1,6 @@
 package com.gucardev.trakyabilmuhbe.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,11 @@ public class BeanConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
