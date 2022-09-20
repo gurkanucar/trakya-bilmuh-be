76b1456420bad57406c5539950b2e6ec6f7f7342
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-20 22:03:59 +0300

    UserService created

---
+++ src/main/java/com/gucardev/trakyabilmuhbe/service/UserService.java
@@ -0,0 +1,48 @@
+package com.gucardev.trakyabilmuhbe.service;
+
+import com.gucardev.trakyabilmuhbe.model.User;
+import com.gucardev.trakyabilmuhbe.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        User existing = userRepository.findById(user.getId())
+                .orElseThrow(() -> new RuntimeException("user not found!"));
+        existing.setExpertises(user.getExpertises());
+        existing.setInterests(user.getInterests());
+        existing.setTitle(user.getTitle());
+        existing.setName(user.getName());
+        existing.setUsername(user.getUsername());
+        return userRepository.save(user);
+    }
+
+    public User setApproved(User user, boolean approved) {
+        User existing = userRepository.findById(user.getId())
+                .orElseThrow(() -> new RuntimeException("user not found!"));
+        existing.setApproved(approved);
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public List<User> getApprovedUsers() {
+        return userRepository.findAllByApprovedTrue();
+    }
+
+
+}
