8fb4d41e38deb1143ab9272d0a3d13699f228059
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-20 22:04:19 +0300

    Notification and Auth service created

---
+++ src/main/java/com/gucardev/trakyabilmuhbe/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.gucardev.trakyabilmuhbe.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+
+
+
+}
