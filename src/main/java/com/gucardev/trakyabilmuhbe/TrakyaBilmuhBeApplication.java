d1985c284e30d5fbbce727785721aa1a9fe4632c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-20 21:17:06 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/trakyabilmuhbe/TrakyaBilmuhBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.trakyabilmuhbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakyaBilmuhBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrakyaBilmuhBeApplication.class, args);
+    }
+
+}
