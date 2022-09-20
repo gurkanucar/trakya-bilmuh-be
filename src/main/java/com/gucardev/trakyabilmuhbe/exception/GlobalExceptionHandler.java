2fcaecec295b52981418b9bf8aa20a4fb31d439a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-20 22:03:35 +0300

    GlobalExceptionHandler created

---
+++ src/main/java/com/gucardev/trakyabilmuhbe/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.gucardev.trakyabilmuhbe.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+
+}
