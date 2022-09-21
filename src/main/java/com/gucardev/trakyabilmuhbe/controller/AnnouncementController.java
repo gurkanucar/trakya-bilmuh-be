e3c259a9e31532aecfc63dbcdbf7ad239aee60a4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-21 21:00:45 +0300

    AnnouncementRequest model created and implemented to controller

--- src/main/java/com/gucardev/trakyabilmuhbe/controller/AnnouncementController.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/controller/AnnouncementController.java
@@ -3,6 +3,7 @@ package com.gucardev.trakyabilmuhbe.controller;
 import com.gucardev.trakyabilmuhbe.dto.AnnouncementDto;
 import com.gucardev.trakyabilmuhbe.dto.MessageDto;
 import com.gucardev.trakyabilmuhbe.model.Announcement;
+import com.gucardev.trakyabilmuhbe.request.AnnouncementRequest;
 import com.gucardev.trakyabilmuhbe.service.AnnouncementService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -29,12 +30,14 @@ public class AnnouncementController {
     }
 
     @PostMapping
-    public ResponseEntity<AnnouncementDto> create(@RequestBody Announcement announcement) {
+    public ResponseEntity<AnnouncementDto> create(@RequestBody AnnouncementRequest announcementRequest) {
+        var announcement = modelMapper.map(announcementRequest, Announcement.class);
         return ResponseEntity.ok(modelMapper.map(announcementService.create(announcement), AnnouncementDto.class));
     }
 
     @PutMapping
-    public ResponseEntity<AnnouncementDto> update(@RequestBody Announcement announcement) {
+    public ResponseEntity<AnnouncementDto> update(@RequestBody AnnouncementRequest announcementRequest) {
+        var announcement = modelMapper.map(announcementRequest, Announcement.class);
         return ResponseEntity.ok(modelMapper.map(announcementService.update(announcement), AnnouncementDto.class));
     }
 
---
+++ src/main/java/com/gucardev/trakyabilmuhbe/request/AnnouncementRequest.java
@@ -0,0 +1,10 @@
+package com.gucardev.trakyabilmuhbe.request;
+
+import lombok.Data;
+
+@Data
+public class AnnouncementRequest {
+    private String title;
+    private String content;
+    private String link;
+}
