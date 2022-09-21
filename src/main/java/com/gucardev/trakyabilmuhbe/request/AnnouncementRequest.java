e3b34d1ecbb499e4e4afb4cd19057b799d772df2
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-21 21:12:33 +0300

    request models validated

--- src/main/java/com/gucardev/trakyabilmuhbe/request/AnnouncementRequest.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/request/AnnouncementRequest.java
@@ -2,9 +2,18 @@ package com.gucardev.trakyabilmuhbe.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnnouncementRequest {
+    @NotNull
+    @NotEmpty
     private String title;
+    @NotNull
+    @NotEmpty
     private String content;
+    @NotNull
+    @NotEmpty
     private String link;
 }
--- src/main/java/com/gucardev/trakyabilmuhbe/request/MessageRequest.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/request/MessageRequest.java
@@ -4,11 +4,19 @@ import com.gucardev.trakyabilmuhbe.model.User;
 import com.gucardev.trakyabilmuhbe.model.notification.MessageType;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 
 @Data
 public class MessageRequest {
+    @NotNull
     private User user;
+    @NotNull
+    @NotEmpty
     private MessageType messageType;
+    @NotNull
+    @NotEmpty
     private String content;
     private String link;
 }
