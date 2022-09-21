1c5aa0532da364b8d3d665b4cf69b3b2e1f76b60
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-21 21:12:16 +0300

    dto models updated

--- src/main/java/com/gucardev/trakyabilmuhbe/dto/AnnouncementDto.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/dto/AnnouncementDto.java
@@ -2,9 +2,14 @@ package com.gucardev.trakyabilmuhbe.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 public class AnnouncementDto {
     private String content;
     private String link;
     private String title;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime updatedDateTime;
 }
--- src/main/java/com/gucardev/trakyabilmuhbe/dto/MessageDto.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/dto/MessageDto.java
@@ -3,10 +3,14 @@ package com.gucardev.trakyabilmuhbe.dto;
 import com.gucardev.trakyabilmuhbe.model.notification.MessageType;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class MessageDto {
     private UserDto user;
     private MessageType messageType;
     private String content;
     private String link;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime updatedDateTime;
 }
