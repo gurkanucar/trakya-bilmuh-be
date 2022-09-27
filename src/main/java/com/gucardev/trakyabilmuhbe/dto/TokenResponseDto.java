5b2b12e88cd4c16559a24a88e015c10638ec432c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 20:51:53 +0300

    TokenResponseDto updated

--- src/main/java/com/gucardev/trakyabilmuhbe/dto/TokenResponseDto.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/dto/TokenResponseDto.java
@@ -1,9 +1,15 @@
 package com.gucardev.trakyabilmuhbe.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class TokenResponseDto {
     private String accessToken;
-    private UserDto userDto;
+    private UserDto user;
 }
