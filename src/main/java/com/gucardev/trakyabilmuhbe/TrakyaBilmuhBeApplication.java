0fc76021afed1c31ec548a5be4c3cf54adf0205a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-06 19:49:45 +0300

    aop dependency added to project

--- src/main/java/com/gucardev/trakyabilmuhbe/TrakyaBilmuhBeApplication.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/TrakyaBilmuhBeApplication.java
@@ -2,8 +2,10 @@ package com.gucardev.trakyabilmuhbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class TrakyaBilmuhBeApplication {
 
     public static void main(String[] args) {
