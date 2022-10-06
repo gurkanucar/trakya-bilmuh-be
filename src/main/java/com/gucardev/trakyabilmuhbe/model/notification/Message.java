299284dccde8a3506205d02b5e4eb818b8faa98e
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-06 20:00:39 +0300

    Message model updated

--- src/main/java/com/gucardev/trakyabilmuhbe/model/notification/Message.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/model/notification/Message.java
@@ -2,7 +2,10 @@ package com.gucardev.trakyabilmuhbe.model.notification;
 
 import com.gucardev.trakyabilmuhbe.model.BaseEntity;
 import com.gucardev.trakyabilmuhbe.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
@@ -12,6 +15,9 @@ import javax.persistence.ManyToOne;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message extends BaseEntity {
 
     @ManyToOne
