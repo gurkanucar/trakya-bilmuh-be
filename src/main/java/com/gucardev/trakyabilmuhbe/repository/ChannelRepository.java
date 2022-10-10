acb303e6d38ed27bb93c3fa276fb176ad6a10af7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-10 21:10:19 +0300

    findChannelsByUser query added to channel repository

--- src/main/java/com/gucardev/trakyabilmuhbe/repository/ChannelRepository.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/repository/ChannelRepository.java
@@ -1,10 +1,15 @@
 package com.gucardev.trakyabilmuhbe.repository;
 
 import com.gucardev.trakyabilmuhbe.model.Channel;
+import com.gucardev.trakyabilmuhbe.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
+
+    List<Channel> findAllByUser(User user);
 
 }
