c4cbfa412ac7f50d956414e6967ee447e889c11d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-10 20:48:03 +0300

    Channel controller and service created

---
+++ src/main/java/com/gucardev/trakyabilmuhbe/controller/ChannelController.java
@@ -0,0 +1,47 @@
+package com.gucardev.trakyabilmuhbe.controller;
+
+import com.gucardev.trakyabilmuhbe.dto.ChannelDto;
+import com.gucardev.trakyabilmuhbe.request.ChannelRequest;
+import com.gucardev.trakyabilmuhbe.service.ChannelService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/channel")
+@RequiredArgsConstructor
+public class ChannelController {
+
+    private final ChannelService channelService;
+    private final ModelMapper modelMapper;
+
+
+    @GetMapping
+    public ResponseEntity<List<ChannelDto>> getChannelList() {
+        return ResponseEntity.ok(channelService.getAllChannels()
+                .stream()
+                .map(x -> modelMapper.map(x, ChannelDto.class))
+                .collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<ChannelDto> createChannel(ChannelRequest channelRequest) {
+        return ResponseEntity.ok(modelMapper.map(channelService.create(channelRequest), ChannelDto.class));
+    }
+
+    @PutMapping
+    public ResponseEntity<ChannelDto> updateChannel(ChannelRequest channelRequest) {
+        return ResponseEntity.ok(modelMapper.map(channelService.update(channelRequest), ChannelDto.class));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> updateChannel(@PathVariable Long id) {
+        channelService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/gucardev/trakyabilmuhbe/service/ChannelService.java
@@ -0,0 +1,62 @@
+package com.gucardev.trakyabilmuhbe.service;
+
+import com.gucardev.trakyabilmuhbe.exception.GeneralException;
+import com.gucardev.trakyabilmuhbe.exception.PermissionError;
+import com.gucardev.trakyabilmuhbe.model.Channel;
+import com.gucardev.trakyabilmuhbe.repository.ChannelRepository;
+import com.gucardev.trakyabilmuhbe.request.ChannelRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ChannelService {
+
+    private final ChannelRepository channelRepository;
+    private final AuthService authService;
+
+
+    public List<Channel> getAllChannels() {
+        return channelRepository.findAll();
+    }
+
+    public Channel create(ChannelRequest channelRequest) {
+
+        String channelTopic = "";
+
+        Channel channel = Channel.builder()
+                .channelTopic(channelTopic)
+                .channelName(channelRequest.getChannelName())
+                .user(channelRequest.getUser())
+                .channelImageUrl(channelRequest.getChannelImageUrl())
+                .build();
+        return channelRepository.save(channel);
+    }
+
+    public Channel update(ChannelRequest channelRequest) {
+        Channel channel = getByID(channelRequest.getId());
+        channel.setChannelName(channel.getChannelName());
+        channel.setChannelImageUrl(channel.getChannelImageUrl());
+        return channelRepository.save(channel);
+    }
+
+
+    public void delete(Long id) {
+        Channel existing = getByID(id);
+        if (!authService.checkForPermission(existing.getUser().getId())) {
+            throw new PermissionError("Permission not granted!");
+        }
+        channelRepository.delete(existing);
+    }
+
+
+    public Channel getByID(Long id) {
+        return channelRepository.findById(id)
+                .orElseThrow(() -> new GeneralException("channel not found!", HttpStatus.NOT_FOUND));
+    }
+}
