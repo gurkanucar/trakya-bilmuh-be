359c39b4f2603057b807fa91072d7e12d13339be
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-13 08:17:15 +0300

    commented codes removed

--- src/main/java/com/gucardev/trakyabilmuhbe/aspect/MessageServiceAspect.java
+++ src/main/java/com/gucardev/trakyabilmuhbe/aspect/MessageServiceAspect.java
@@ -1,14 +1,11 @@
 package com.gucardev.trakyabilmuhbe.aspect;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gucardev.trakyabilmuhbe.model.NotificationMessage;
 import com.gucardev.trakyabilmuhbe.model.Message;
+import com.gucardev.trakyabilmuhbe.model.NotificationMessage;
 import com.gucardev.trakyabilmuhbe.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.modelmapper.ModelMapper;
@@ -22,14 +19,6 @@ public class MessageServiceAspect {
 
     private final ModelMapper mapper;
     private final NotificationService notificationService;
-
-//    @After(value = "execution(* com.gucardev.trakyabilmuhbe.service.MessageService.create(..)) && args(message)")
-//    public void afterSaveMessage(JoinPoint joinPoint, Message message) {
-//        notificationService.sendNotification(NotificationMessage.builder()
-//                .title(message.getChannel().getChannelName())
-//                .topic(message.getChannel().getChannelTopic())
-//                .content(message.getContent()).build());
-//    }
 
     @Around("execution(* com.gucardev.trakyabilmuhbe.service.MessageService.create(..))")
     public Object afterSaveMessage(ProceedingJoinPoint pjp) throws Throwable {
