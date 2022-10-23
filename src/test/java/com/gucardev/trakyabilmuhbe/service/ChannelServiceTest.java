fd508c8f3f3557e1b9d008622fc33f9f8286ddbf
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-23 19:15:16 +0300

    updateChannelTest written

--- src/test/java/com/gucardev/trakyabilmuhbe/service/ChannelServiceTest.java
+++ src/test/java/com/gucardev/trakyabilmuhbe/service/ChannelServiceTest.java
@@ -1,5 +1,6 @@
 package com.gucardev.trakyabilmuhbe.service;
 
+import com.gucardev.trakyabilmuhbe.exception.PermissionError;
 import com.gucardev.trakyabilmuhbe.model.Channel;
 import com.gucardev.trakyabilmuhbe.model.User;
 import com.gucardev.trakyabilmuhbe.repository.ChannelRepository;
@@ -11,7 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,6 +54,76 @@ class ChannelServiceTest {
         verify(channelRepository, times(1)).save(expected);
         assertEquals(expected, actual);
 
+
+    }
+
+    @Test
+    void test_updateChannel_whenChannelExistsAndPermissionGranted_thenReturn_updatedChannel() {
+        ChannelRequest channelRequest = ChannelRequest.builder()
+                .id(1L)
+                .channelName("update_channelName")
+                .canSendOthers(true)
+                .channelImageUrl("update_imageUrl")
+                .user(User.builder().id(1L).username("user").build())
+                .build();
+        Channel existing = Channel.builder()
+                .id(1L)
+                .canSendOthers(false)
+                .channelName("channelName")
+                .user(User.builder().id(1L).username("user").build())
+                .channelImageUrl("imageUrl")
+                .build();
+
+        Channel expected = Channel.builder()
+                .id(1L)
+                .canSendOthers(false)
+                .channelName("channelName")
+                .user(User.builder().id(1L).username("user").build())
+                .channelImageUrl(channelRequest.getChannelImageUrl())
+                .build();
+
+        when(channelRepository.save(existing)).thenReturn(expected);
+        when(channelRepository.findById(1L)).thenReturn(Optional.ofNullable(existing));
+        when(authService.checkForPermission(channelRequest.getUser().getId())).thenReturn(true);
+
+        var actual = channelService.update(channelRequest);
+
+
+        verify(channelRepository, times(1)).save(expected);
+        verify(channelRepository, times(1)).findById(channelRequest.getId());
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void test_updateChannel_whenChannelExistsAndPermissionNotGranted_then_throwException() {
+        ChannelRequest channelRequest = ChannelRequest.builder()
+                .id(1L)
+                .channelName("update_channelName")
+                .canSendOthers(true)
+                .channelImageUrl("update_imageUrl")
+                .user(User.builder().id(1L).username("user").build())
+                .build();
+        Channel existing = Channel.builder()
+                .id(1L)
+                .canSendOthers(false)
+                .channelName("channelName")
+                .user(User.builder().id(1L).username("user").build())
+                .channelImageUrl("imageUrl")
+                .build();
+
+        when(channelRepository.findById(1L)).thenReturn(Optional.ofNullable(existing));
+        when(authService.checkForPermission(channelRequest.getUser().getId())).thenReturn(false);
+
+        var error = assertThrows(PermissionError.class, () -> channelService.update(channelRequest));
+
+
+        assert existing != null;
+        verify(channelRepository, times(0)).save(existing);
+        verify(channelRepository, times(1)).findById(channelRequest.getId());
+
+        assertEquals(PermissionError.class, error.getClass());
 
     }
 
